ae081df2ce9ed3fca9d4c32c9d2234082d92af5a
Abdeladim <abdeladim.laaroua@gmail.com>
2017-04-24 23:50:15 +0100

    edit v1.0

--- gestion/dist/js/list.js
+++ gestion/dist/js/list.js
@@ -3,6 +3,12 @@ $(function () {
   $('#loading-image').popup({
     blur: false
   });
+  //Date picker
+  $('#naissance_edit').datepicker({
+    autoclose: true,
+    format: 'yyyy-mm-dd'
+  });
+
   var table;
   var obj = {'methode' : 'getAllClient'}
     $.ajax({
@@ -39,6 +45,44 @@ $(function () {
         });
 
         table = $("#table_clients").DataTable();
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(errorThrown);
+      }
+    });
+    var obj_city = {'methode' : 'getAllCities'}
+    $.ajax({
+      url : "./lib/util.php",
+      dataType: "json",
+      type: "POST",
+      data : obj_city,
+      success: function(data, textStatus, jqXHR) {
+        console.log(data.result);
+        $.each(data.result, function(key, val) {
+          var newOption = '<option value="'+val.id+'">'+val.name+'</option>';
+          $("#Ville_id_edit").append(newOption);
+        });
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(errorThrown);
+      }
+    });
+    var obj_type = {'methode' : 'getAllTypeMoms'}
+    $.ajax({
+      url : "./lib/util.php",
+      dataType: "json",
+      type: "POST",
+      data : obj_type,
+      success: function(data, textStatus, jqXHR) {
+        console.log(data.result);
+        $.each(data.result, function(key, val) {
+          var newOption = '<option value="'+val.type+'">'+val.type+'</option>';
+          $("#type_edit").append(newOption);
+        });
       },
       error: function (jqXHR, textStatus, errorThrown) {
         console.log(jqXHR);
@@ -108,7 +152,62 @@ $(function () {
         });
         break;
       case 'edit' :
-        console.log('edit');
+        console.log('edit');var get_client = {"methode":"getClient","id":id};
+        $.ajax({
+          url : "./lib/util.php",
+          dataType: "json",
+          type: "POST",
+          data : get_client,
+          beforeSend: function(){
+            $('#loading-image').popup('show');
+          },
+          complete: function(){
+            setTimeout(function(){$('#loading-image').popup('hide');},250);
+          },
+          success: function(data, textStatus, jqXHR) {
+            $.validate();
+            $('.infos_client').text('');
+            $("#baby_table tbody").empty();
+            $('#Ville_id_edit').val(0);
+            if(data.result != 0){
+              var client = data.result.client[0];
+              console.log(client);
+              $('#id_client_edit').text(client.id_client);
+              $('#nom_edit').val(client.nom);
+              $('#prenom_edit').val(client.prenom);
+              $('#email_edit').val(client.email);
+              $('#gsm_edit').val(client.gsm);
+              $('#naissance_edit').val(client.naissance);
+              $('#adresse_edit').val(client.adresse);
+              $('#CP_edit').val(client.CP);
+              $('#type_edit').val(client.type);
+              $('#Ville_id_edit').val(client.Ville_id);
+              $('#creationDate_edit').text(client.creationDateClient);
+              var babies = data.result.baby;
+              console.log(babies);
+              if(babies.lenght == 0){
+                var newRow = '<tr>'+
+                  '<td>Aucun bébé trouvé</td>'+
+                  '</tr>';
+                  $("#baby_table tbody").append(newRow);
+              }else{
+                $.each(babies, function(key, val) {   
+                  var newRow = '<tr>'+
+                    '<td>'+val.id+'</td>'+
+                    '<td>'+val.prenom+'</td>'+
+                    '<td>'+val.sexe+'</td>'+
+                    '<td>'+val.naissance+'</td>'+
+                    '<td>'+val.MATERNITE+'</td>'+
+                    '</tr>';
+                  $("#baby_table tbody").append(newRow);
+                });
+              }
+            }
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+
+          }
+        });
 
       break;
       case 'delete' :
